c0107e114aab121315d4ec392fbacc801691a654
fire8327 <fire83274@gmail.com>
2025-06-25 17:23:21 +0300

    add pop-up

--- Assets/JS/main.js
+++ Assets/JS/main.js
@@ -1,3 +1,4 @@
+/* slider */
 const mainSwiper = new Swiper('.mainSwiper', {
     loop: true,
     slidesPerView: 1,
@@ -11,3 +12,24 @@ const mainSwiper = new Swiper('.mainSwiper', {
         prevEl: '.prev-swiper',
     }
 });
+
+
+/* modal */
+$('#openModal').on('click', () => {
+    $('#modal-bg').removeClass('hidden');
+    $('#modal').removeClass('-translate-y-[1000px]');
+    $('#modal').addClass('top-1/2 -translate-y-1/2');
+});
+
+$('#modal-bg').on('click', () => {
+    $('#modal-bg').addClass('hidden');
+    $('#modal').addClass('-translate-y-[1000px]');
+    $('#modal').removeClass('top-1/2 -translate-y-1/2');
+});
+
+
+
+
+
+
+
